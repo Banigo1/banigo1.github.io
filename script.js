1ea4e47afa31a579f979033ba5c1e9c7735a3c0b
Precious Banigo <banigoprecious@yahoo.com>
2023-10-25 03:42:17 +0100

    My Portfolio Website

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/**
+  /**
    * Form Validation
    */
 
@@ -18,7 +18,7 @@ function validateName(){
     
 
     if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
-        nameError.innerHTML = 'Write full name';
+        nameError.innerHTML = 'Enter first name and last name only';
         return false;
 
     }
@@ -39,7 +39,7 @@ function validatePhone(){
 
     }
     if(!phone.match(/^[0-10]{11}$/)){
-        phoneError.innerHTML = 'only digits please.';
+        phoneError.innerHTML = '';
         return false;
 }
 phoneError.innerHTML = '<i class="fas fa-check-circle"></i>';
@@ -64,33 +64,30 @@ emailError.innerHTML = '<i class="fas fa-check-circle"></i>';
     return true;
 }
 
-
-
+/*
 function validateMessage(){
     var message = document.getElementById('contact-message').value;
-    var required = 30;
+    var required =30;
     var left = required - message.length;
-
+    
     if (left>0){
-        messageError.innerHTML = left + 'more characters required';
+        messageError.innerHTML = left + ' characters left';
         return false;
     }
+
     messageError.innerHTML = '<i class="fas fa-check-circle"></i>';
     return true;
 }
+    function validateForm(){
+      if(!validateName() || !validatePhone() || !validateEmail() || !validateMessage()){
+        submitError.style.display = 'block';
+        submitError.innerHTML = 'Please fix error to submit';
+        setTimeout(function(){submitError.style.display = 'none';}, 3000);
+        return false;
 
-
-
-function validateForm(){
-   if(!validateName() || !validatePhone() || !validateEmail() || !validateMessage()){
-    submitError.style.display = 'block';
-    submitError.innerHTML = 'Please fix error to submit';
-    setTimeout(function(){submitError.style.display = 'none';}, 3000);
-    return false;
-
-   }
-}
-
+      }
+    }
+    */
 (function() {
   "use strict";
 
